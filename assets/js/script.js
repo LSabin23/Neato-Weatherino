c54ae27298ab66ff15b00b1cb10578d2bfd7aa80
unknown <44147809+LSabin23@users.noreply.github.com>
2021-11-13 10:09:27 -0500

    added functionality for form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-var cityInput = document.querySelector('#city')
+var searchFormEl = document.querySelector('#search-form')
+var cityInputEl = document.querySelector('#city')
 
 var getCurrentWeather = function (city) {
   var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=f8bdae7d507d571fec219a255946d59e'
@@ -9,6 +10,18 @@ var getCurrentWeather = function (city) {
   })
 }
 
-getCurrentWeather('Owosso')
+var formSubmitHandler = function (event) {
+  event.preventDefault()
+  var cityName = cityInputEl.value.trim()
+
+  if (cityName) {
+    getCurrentWeather(cityName)
+    cityInputEl.value = ''
+  } else {
+    alert('Please enter a city name.')
+  }
+}
+
+searchFormEl.addEventListener('submit', formSubmitHandler)
 
 // Open Weather API Key: f8bdae7d507d571fec219a255946d59e
