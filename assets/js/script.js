a0b9ef28ae48d94599d8f5dfcbd3b1e2ed8cebd3
unknown <44147809+LSabin23@users.noreply.github.com>
2021-11-13 09:48:57 -0500

    added successful fetch request to Open Weather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
-console.log('The JS file is connected.')
+var cityInput = document.querySelector('#city')
+
+var getCurrentWeather = function (city) {
+  var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=f8bdae7d507d571fec219a255946d59e'
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data)
+    })
+  })
+}
+
+getCurrentWeather('Owosso')
+
+// Open Weather API Key: f8bdae7d507d571fec219a255946d59e
